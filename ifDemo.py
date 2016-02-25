051bd9eb9cb006bc16fdca5a31bca1f699096c8a
qc179 <179313554@qq.com>
2016-02-25 13:54:39 +0800

    Second commit after arranged

--- ifDemo.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-i=10
-if i>0:
-    print 'i>0'
-elif i==0:
-    print 'i=0'
-else:
-    print 'i<0'
--- tryexcept.py
+++
@@ -1,7 +0,0 @@
-a=5
-try:
-    a=a+1
-    a=a/0
-except:
-    pass
-print a
