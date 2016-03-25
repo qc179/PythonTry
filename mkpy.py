2a98c03957e5b4d1a013b64ce53a209c79906006
qc179 <179313554@qq.com>
2016-03-25 16:01:13 +0800

    mkpy2.0 : will chmod file after create

--- mkpy.py
+++ mkpy.py
@@ -2,18 +2,21 @@
 #-*- coding: utf-8 -*-
 #filename:mkpy.py
 
-import sys
+import sys,os
 
 def mkpy(argv):
 
     for f in argv:
 
         filename=str(f)+'.py'
-        print 'Create',filename,'complate.'
 
         mk=open(filename,'w')
         mk.write('#!/usr/bin/env python\n#-*- coding: utf-8 -*-\n#filename:{}\n'.format(filename))
         mk.close()
+        os.chmod(filename,0774)
+        print 'Create',filename,'complate.'
+
+    print 'Finish.'
 
 if __name__=='__main__':
     mkpy(sys.argv[1:])
