eaf85ac38f96e3dcde1c718ea9d11ee25c5ce957
qc179 <179313554@qq.com>
2016-04-06 17:16:51 +0800

    update csvDemo.py

--- csvDemo.py
+++ csvDemo.py
@@ -5,12 +5,23 @@
 import csv
 
 #def csvDemo():
-rowlist=[]
-with open('aaa.csv','rb') as csvopen:
-    csvread=csv.reader(csvopen,dialect='excel')
-    for row in csvread:
-         rowlist.append(row)
+#rowlist=[]
+with open('aaa.csv','rb') as csvrb:
+    csvread=csv.reader(csvrb,dialect='excel')
+    rows=[row for row in csvread]
+#    for row in csvread:
+#         rowlist.append(row)
 
-for i in range(len(rowlist))[1:]:
-    print rowlist[i]
+for i in rows[1:]:
+    i[1]=i[1].decode('gbk').encode('utf-8')
+    i[2]=i[2].decode('gbk').encode('utf-8')
+    for ii in i:
+        print ii,
+    print '\m'
 
+with open('bbb.csv','ab') as csvab:
+    csvwrite=csv.writer(csvab,dialect='excel')
+    for i in rows[1:]:
+        csvwrite.writerow(i)
+
+quit=raw_input('Enter any word to exit : ')
