e8ee26e5dbc37af25e215c3de76fa2df0ac384bc
qc179 <179313554@qq.com>
2016-04-08 16:10:39 +0800

    xls1.2

--- xls/xlsDemo.py
+++ xls/xlsDemo.py
@@ -7,14 +7,13 @@ import os,time
 try:
     import xlrd
 except Exception, e:
-    print ' Start to setup module xlrd .\n'
+    print ' Start to setup module xlrd.\n'
     os.chdir('xlrd-0.9.4')
-    cwd = os.getcwd()
-    print cwd
-    # os.system('python setup.py install')
-    print '\n Finished .'
+    os.system('python setup.py install')
+    print '\n Finished.'
 else:
-    print '\n The module xlrd was already exists .'
+    print '\n The module xlrd was already exists.'
 
 time.sleep(1)
-getany = raw_input('\n press Enter to quit . ')
+getany = raw_input('\n press Enter to quit. ')
+    
