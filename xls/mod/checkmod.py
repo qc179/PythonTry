df8d86bb12889792c25f45bbb2b85ddb11c7ae18
qc179 <179313554@qq.com>
2016-04-10 10:20:26 +0800

    xls1.7:modcheck test on windows7 pass

--- xls/mod/checkmod.py
+++ xls/mod/checkmod.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-#filename:xlsDemo.py
+#filename:checkmod.py
 #You need root power when install on linux
 
 import os,time
+
+cwd = os.getcwd()
 
 print '\nChecking module xlrd ..'
 
 try:
     import xlrd
 except Exception, e:
-    print '\nStart to setup module xlrd ...\n'
-    os.chdir('xlrd-0.9.4')
+    print '\nStart to setup module xlrd ...'
+    os.chdir(cwd+'\\xlrd-0.9.4')
     os.system('python setup.py install')
     print '\nFinished.'
 else:
@@ -23,12 +25,25 @@ print '\nChecking module xlwt ..'
 try:
     import xlwt
 except Exception, e:
-    print '\nStart to setup module xlwt ...\n'
-    os.chdir('xlwt-1.0.0')
+    print '\nStart to setup module xlwt ...'
+    os.chdir(cwd+'\\xlwt-1.0.0')
     os.system('python setup.py install')
     print '\nFinished.'
 else:
     print '\nThe module xlwt was already exists.'
 time.sleep(1)
 
-getany = raw_input('\npress Enter to quit. ')    
+print '\nChecking module psycopg2 ..'
+
+try:
+    import psycopg2
+except Exception, e:
+    print '\nStart to setup module psycopg2 ...'
+    os.chdir(cwd)
+    os.system('pip install psycopg2-2.6.1-cp27-none-win32.whl')
+    print '\nFinished.'
+else:
+    print '\nThe module psycopg2 was already exists.'
+time.sleep(1)
+
+getany = raw_input('\npress Enter to quit. ')
