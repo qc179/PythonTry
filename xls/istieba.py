b0b2392e6501aeda4062f016718ace6623d09bdd
qc179 <179313554@qq.com>
2016-05-09 15:49:54 +0800

    Update istieba mod surpport much judge
    Add two parts of new code to judge name and url of tieba in istieba.py
    The istieba.py has been test recently.

--- xls/istieba.py
+++ xls/istieba.py
@@ -16,7 +16,13 @@ for row in range(bsheet1.nrows)[1:]:
         values.append(bsheet1.cell(row,col).value)
     srclist.append(values)
 
-
+inittieba = xlwt.Workbook(encoding='utf-8')
+tieba = inittieba.add_sheet('sheet1')
+tieba.write(0,0,'Name')
+tieba.write(0,1,'Url')
+tieba.write(0,2,'Status')
+tieba.col(1).width = 256*50
+tieba.col(0).width = 256*15
 
 #judge each source whether has been inserted , then save it
 print 'There are {} boards need to be checked ..'.format(len(srclist))
@@ -26,23 +32,33 @@ for eachsrc in srclist:
     #eachsrc[0]:board name
     #eachsrc[1]:board url
     print 'Source number:',rows
-    print 'Name:'+eachsrc[0]
     istieba = re.match('http://tieba\.baidu\.com.*',eachsrc[1])
     if istieba:
-        print 'URL:'+eachsrc[1]+' is tieba'
         namelen = len(eachsrc[0])
         namelast = eachsrc[0][namelen-1]
-        if namelast == u'吧' :
-            print u'yes,it is 吧'
+        urlfmt = re.match('^http://tieba\.baidu\.com/f\?kw=[A-za-z0-9%]+$',\
+            eachsrc[1])
+        if namelast != u'吧' :
+            tieba.write(rows,0,eachsrc[0])
+            tieba.write(rows,1,eachsrc[1])
+            tieba.write(rows,2,u'吧名不规范，请检查')
         else :
-            #here need update to result after
-            print u'吧名末尾不是“吧”字，请修正。'
+            if urlfmt:
+                tieba.write(rows,0,eachsrc[0])
+                tieba.write(rows,1,eachsrc[1])
+                tieba.write(rows,2,u'吧名和URL正确')
+            else:
+                tieba.write(rows,0,eachsrc[0])
+                tieba.write(rows,1,eachsrc[1])
+                tieba.write(rows,2,u'URL不规范，请检查')
+
     else:
-        print 'URL:'+eachsrc[1]+' is not a tieba'
+        tieba.write(rows,0,eachsrc[0])
+        tieba.write(rows,1,eachsrc[1])
+        tieba.write(rows,2,u'非贴吧URL')
     rows = rows+1
-    print '\n'
 
-
+inittieba.save('tiebaresult.xls')
 
 print '*'*75
 anyenter = raw_input('Check result has been saved.Press Enter to quit.')
