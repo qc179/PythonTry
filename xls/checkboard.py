0087e4790ccdc9a45c9a3e10c27769d7f13af0c5
qc179 <179313554@qq.com>
2016-05-10 14:13:45 +0800

    Merge istieba into checkboard and test complete
    Because of the specificity of tieba's URL,now checkboard has two ways to check a source of board:tieba or not tieba.If one source is a tieba,then check whether name and url are both right.

--- xls/checkboard.py
+++ xls/checkboard.py
@@ -2,24 +2,16 @@
 #-*- coding: utf-8 -*-
 #filename:checkboard.py
 
+
 import sys
 sys.path.append('mod')
 import xlrd
 import xlwt
 import psycopg2 as pg2
 import getcfg
+import re
 
 cfg = getcfg.getcfg('checkboard.cfg')
-
-#make board source into a list from sheet1
-board = xlrd.open_workbook('board.xls')
-bsheet1 = board.sheets()[0]
-srclist = []
-for row in range(bsheet1.nrows)[1:]:
-    values = []
-    for col in range(bsheet1.ncols):
-        values.append(bsheet1.cell(row,col).value)
-    srclist.append(values)
 
 #try connect database
 try:
@@ -30,13 +22,21 @@ try:
     host=cfg['host'],
     port=cfg['port']
     )
+    cur = conn.cursor()
 except Exception,e:
     #print e
     print 'Connect server failed.'
     anyenter = raw_input('Press ENTER to quit.')
 
-#make cursor of connection 'conn'
-cur = conn.cursor()
+#make board source into a list from sheet1
+board = xlrd.open_workbook('board.xls')
+bsheet1 = board.sheets()[0]
+srclist = []
+for row in range(bsheet1.nrows)[1:]:
+    values = []
+    for col in range(bsheet1.ncols):
+        values.append(bsheet1.cell(row,col).value)
+    srclist.append(values)
 
 #init result.xls such as title and column width
 initsave = xlwt.Workbook(encoding='utf-8')
@@ -50,37 +50,90 @@ isheet1.write(0,5,'Count')
 isheet1.col(2).width = 256*50
 isheet1.col(1).width = 256*15
 
-#judge each source whether has been inserted , then save it
+def checktieba(tbname,tburl):
+    namelen = len(tbname)
+    namelast = tbname[namelen-1]
+    urlfmt = re.match('^http://tieba\.baidu\.com/f\?kw=[A-za-z0-9%]+$',\
+                      tburl)
+    try:
+        if namelast != u'吧' :
+            a = 1
+            # tieba.write(rows,2,u'吧名不规范，请检查')
+        else :
+            if urlfmt:
+                a = 0
+                # tieba.write(rows,2,u'吧名和URL正确')
+            else:
+                a = 2
+                # tieba.write(rows,2,u'URL不规范，请检查')
+    except Exception, e:
+        a = 9
+        return a
+    return a
+
+#judge each source whether has been inserted,then save them
 print 'There are {} boards need to be checked ..'.format(len(srclist))
 print '*'*78
 rows = 1
 for eachsrc in srclist:
     #eachsrc[0]:board name
     #eachsrc[1]:board url
-    sql0 = "select fid,name,url,bid from board where is_active=1 \
-    and name~'"+eachsrc[0]+"' and url='"+eachsrc[1]+"' order by bid"
-    cur.execute(sql0)
-    anslist = cur.fetchall()
-    #anslist:a list of select results    
-    if len(anslist) == 0:
-        #isheet1.write(rows,0,'NONE')
-        isheet1.write(rows,1,eachsrc[0])
-        isheet1.write(rows,2,eachsrc[1])
-        # isheet1.write(rows,3,'NONE')
-        isheet1.write(rows,4,'未配置')
-        isheet1.write(rows,5,len(anslist))
-        print 'complete {}/{}.'.format(rows,len(srclist))
-        rows = rows+1
+    istieba = re.match('http://tieba\.baidu\.com.*',eachsrc[1])
+    if istieba:
+        cktieba = checktieba(eachsrc[0],eachsrc[1])
+        if cktieba == 9:
+            isheet1.write(rows,1,eachsrc[0])
+            isheet1.write(rows,2,eachsrc[1])
+            isheet1.write(rows,4,u'判断过程中出错')
+        elif cktieba == 1:
+            isheet1.write(rows,1,eachsrc[0])
+            isheet1.write(rows,2,eachsrc[1])
+            isheet1.write(rows,4,u'吧名不规范，请检查')
+        elif cktieba == 2:
+            isheet1.write(rows,1,eachsrc[0])
+            isheet1.write(rows,2,eachsrc[1])
+            isheet1.write(rows,4,u'URL不规范，请检查')
+        else:
+            sqltieba = "select fid,name,url,bid from board where \
+            is_active=1 and fid=101 and name='"+eachsrc[0]+"' order by bid"
+            cur.execute(sqltieba)
+            anslist = cur.fetchall()   
+            if len(anslist) == 0:
+                isheet1.write(rows,1,eachsrc[0])
+                isheet1.write(rows,2,eachsrc[1])
+                isheet1.write(rows,4,u'未配置')
+                isheet1.write(rows,5,len(anslist))
+            else:
+                ans = anslist[0]
+                isheet1.write(rows,0,ans[0])
+                isheet1.write(rows,1,ans[1])
+                isheet1.write(rows,2,ans[2])
+                isheet1.write(rows,3,ans[3])
+                isheet1.write(rows,4,'OK')
+                isheet1.write(rows,5,len(anslist))            
     else:
-        ans = anslist[0]
-        isheet1.write(rows,0,ans[0])
-        isheet1.write(rows,1,ans[1])
-        isheet1.write(rows,2,ans[2])
-        isheet1.write(rows,3,ans[3])
-        isheet1.write(rows,4,'OK')
-        isheet1.write(rows,5,len(anslist))
-        print 'complete {}/{}.'.format(rows,len(srclist))
-        rows = rows+1
+        sql0 = "select fid,name,url,bid from board where is_active=1 \
+        and name~'"+eachsrc[0]+"' and url='"+eachsrc[1]+"' order by bid"
+        cur.execute(sql0)
+        anslist = cur.fetchall()
+        #anslist:a list of select results    
+        if len(anslist) == 0:
+            #isheet1.write(rows,0,'NONE')
+            isheet1.write(rows,1,eachsrc[0])
+            isheet1.write(rows,2,eachsrc[1])
+            # isheet1.write(rows,3,'NONE')
+            isheet1.write(rows,4,u'未配置')
+            isheet1.write(rows,5,len(anslist))
+        else:
+            ans = anslist[0]
+            isheet1.write(rows,0,ans[0])
+            isheet1.write(rows,1,ans[1])
+            isheet1.write(rows,2,ans[2])
+            isheet1.write(rows,3,ans[3])
+            isheet1.write(rows,4,'OK')
+            isheet1.write(rows,5,len(anslist))
+    print 'complete {}/{}.'.format(rows,len(srclist))
+    rows = rows+1
 
 initsave.save('result.xls')
 
--- xls/istieba.py
+++ xls/istieba.py
@@ -24,6 +24,24 @@ tieba.write(0,2,'Status')
 tieba.col(1).width = 256*50
 tieba.col(0).width = 256*15
 
+def checktieba(tbname,tburl):
+    namelen = len(tbname)
+    namelast = tbname[namelen-1]
+    urlfmt = re.match('^http://tieba\.baidu\.com/f\?kw=[A-za-z0-9%]+$',\
+                      tburl)
+    a = 9
+    if namelast != u'吧' :
+        a = 1
+        # tieba.write(rows,2,u'吧名不规范，请检查')
+    else :
+        if urlfmt:
+            a = 0
+            # tieba.write(rows,2,u'吧名和URL正确')
+        else:
+            a = 2
+            # tieba.write(rows,2,u'URL不规范，请检查')
+    return a
+
 #judge each source whether has been inserted , then save it
 print 'There are {} boards need to be checked ..'.format(len(srclist))
 print '*'*75
@@ -34,31 +52,28 @@ for eachsrc in srclist:
     print 'Source number:',rows
     istieba = re.match('http://tieba\.baidu\.com.*',eachsrc[1])
     if istieba:
-        namelen = len(eachsrc[0])
-        namelast = eachsrc[0][namelen-1]
-        urlfmt = re.match('^http://tieba\.baidu\.com/f\?kw=[A-za-z0-9%]+$',\
-            eachsrc[1])
-        if namelast != u'吧' :
+        cktieba = checktieba(eachsrc[0],eachsrc[1])
+        if cktieba == 0:
+            tieba.write(rows,0,eachsrc[0])
+            tieba.write(rows,1,eachsrc[1])
+            tieba.write(rows,2,u'吧名和URL正确')
+        elif cktieba == 1:
             tieba.write(rows,0,eachsrc[0])
             tieba.write(rows,1,eachsrc[1])
             tieba.write(rows,2,u'吧名不规范，请检查')
-        else :
-            if urlfmt:
-                tieba.write(rows,0,eachsrc[0])
-                tieba.write(rows,1,eachsrc[1])
-                tieba.write(rows,2,u'吧名和URL正确')
-            else:
-                tieba.write(rows,0,eachsrc[0])
-                tieba.write(rows,1,eachsrc[1])
-                tieba.write(rows,2,u'URL不规范，请检查')
-
+        elif cktieba == 2:
+            tieba.write(rows,0,eachsrc[0])
+            tieba.write(rows,1,eachsrc[1])
+            tieba.write(rows,2,u'URL不规范，请检查')
+        else:
+            pass
     else:
         tieba.write(rows,0,eachsrc[0])
         tieba.write(rows,1,eachsrc[1])
         tieba.write(rows,2,u'非贴吧URL')
     rows = rows+1
 
-inittieba.save('tiebaresult.xls')
+inittieba.save('checkresult.xls')
 
 print '*'*75
 anyenter = raw_input('Check result has been saved.Press Enter to quit.')
